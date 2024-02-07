f429c86ea2cabbc399fea140ddf4a5c3062c6e21
Sondos <sondosahmedabed@gmail.com>
2024-02-08 00:10:57 +0200

    Edit userQuery

--- app/queries/userQuery.py
+++ app/queries/userQuery.py
@@ -1,21 +1,22 @@
-from flask import jsonify
-from flask import request
-from flask import Blueprint
+from app import app
+from flask import jsonify, request
 from app.queries.mysqlconnect import execute_query
+from flask import Blueprint
 
-bp = Blueprint('/user', __name__)
+bp = Blueprint('user', __name__)
 
-@bp.route('/students', methods=['POST'])
+@app.route('/studentsI', methods=['POST'])
 def add_student():
     try:
         data = request.json
+        student_id = data['student_id']
         name = data['name']
         email = data['email']
         profile_pic = data['profile_pic']
         password = data['password']
 
-        query = "INSERT INTO student (name, email, profile_pic, password) VALUES (%s, %s, %s, %s)"
-        execute_query(query, (name, email, profile_pic, password))
+        query = "INSERT INTO student (student_id, name, email, profile_pic, password) VALUES (%s, %s, %s, %s, %s)"
+        execute_query(query, (student_id, name, email, profile_pic, password), commit=True)
 
         response = jsonify({'message': 'Student information inserted successfully'})
         response.status_code = 200
@@ -26,11 +27,11 @@ def add_student():
         response.status_code = 500
         return response
 
-@bp.route('/students/<int:student_id>', methods=['DELETE'])
+@app.route('/students/<int:student_id>', methods=['DELETE'])
 def delete_student(student_id):
     try:
         query = "DELETE FROM student WHERE student_id = %s"
-        execute_query(query, (student_id,))
+        execute_query(query, (student_id,), commit=True)
 
         response = jsonify({'message': 'Student account deleted successfully'})
         response.status_code = 200
@@ -41,7 +42,7 @@ def delete_student(student_id):
         response.status_code = 500
         return response
 
-@bp.route('/students', methods=['PUT'])
+@app.route('/studentsU', methods=['PUT'])
 def update_student():
     try:
         data = request.json
@@ -52,7 +53,7 @@ def update_student():
         password = data['password']
 
         query = "UPDATE student SET name=%s, email=%s, profile_pic=%s, password=%s WHERE student_id=%s"
-        execute_query(query, (name, email, profile_pic, password, student_id))
+        execute_query(query, (name, email, profile_pic, password, student_id), commit=True)
 
         response = jsonify({'message': 'Student information updated successfully'})
         response.status_code = 200
@@ -63,7 +64,7 @@ def update_student():
         response.status_code = 500
         return response
 
-@bp.route('/students', methods=['GET'])
+@app.route('/studentsS')
 def get_students():
     try:
         query = "SELECT * FROM student"
@@ -77,3 +78,19 @@ def get_students():
         response = jsonify({'error': 'An error occurred while fetching student information'})
         response.status_code = 500
         return response
+
+@app.route('/students/<int:id>', methods=['GET'])
+def get_student_by_id(id):
+    try:
+        query = "SELECT * FROM student WHERE id = %s"
+        student_row = execute_query(query, (id,), fetch_one=True)
+
+        if student_row:
+            response = jsonify(student_row)
+            response.status_code = 200
+            return response
+        else:
+            return jsonify({"message": "Student not found"}), 404
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "Internal Server Error"}), 500
