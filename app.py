727747f392dd06c28e8929c993bdb4ff7ad9398f
Sondos <sondosahmedabed@gmail.com>
2024-02-07 03:44:26 +0200

    Register blueprints to the app

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+import projectQuery
+import taskQuery
+import teamQuery
+import userQuery
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    """
+        Import quires and add the blue prints to
+        the app 
+        Return:
+            initialized app
+    """
+    app = Flask(__name__)
+    CORS(app)
+    app.register_blueprint(projectQuery.bp)
+    app.register_blueprint(taskQuery.bp)
+    app.register_blueprint(teamQuery.bp)
+    app.register_blueprint(userQuery.bp)
+    return app
