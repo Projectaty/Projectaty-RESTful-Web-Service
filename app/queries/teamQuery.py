aca2afc5eea48047e094581b668b089375692bca
Sondos Ahmad Aabed <65151701+sondosaabed@users.noreply.github.com>
2024-02-09 00:47:29 +0200

    Update teamQuery.py

--- app/queries/teamQuery.py
+++ app/queries/teamQuery.py
@@ -1,6 +1,164 @@
-from flask import jsonify
-from flask import request
-from flask import Blueprint
+from flask import jsonify, request, Blueprint
 from app.queries.mysqlconnect import execute_query
 
 bp = Blueprint('team', __name__)
+
+# Get team data by ID
+@bp.route('/team/<int:team_id>', methods=['GET'])
+def get_team_by_id(team_id):
+    try:
+        query = "SELECT * FROM Team WHERE TeamID = %s"
+        team_row = execute_query(query, (team_id), fetch_one=True)
+
+        if team_row:
+            response = jsonify(team_row)
+            response.status_code = 200
+            return response
+        else:
+            return jsonify({"message": "team not found"}), 404
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "Internal Server Error"}), 500
+    
+# Add Team data
+@bp.route('/add', methods=['POST'])
+def add_team():
+    try:
+        """ Get the data from json request """
+        data = request.json
+        team_id = data['TeamID']
+        team_name = data['TeamName']
+        
+        query = "INSERT INTO Team (TeamID, TeamName) VALUES (%s, %s)"
+        execute_query(query, (team_id, team_name), commit=True)
+        
+        """ If inserted succesfuly return a message"""
+        response = jsonify({'message': 'Team information inserted successfully'})
+        response.status_code = 200
+        return response
+    except Exception as e:
+        """ Hadnle exceptions by printing the errors and put them in the response """
+        print(e)
+        response = jsonify({'error': 'An error occurred while inserting team information'})
+        response.status_code = 500
+        return response
+    
+# Delete team by ID
+@bp.route('/delete/<int:team_id>', methods=['DELETE'])
+def delete_team(team_id):
+
+    try:
+        query = "DELETE FROM Team WHERE TeamID = %s"
+        execute_query(query, (team_id,), commit=True)
+
+        """ Return respose message if successfuly deletd """
+        response = jsonify({'message': 'Team deleted successfully'})
+        response.status_code = 200
+        return response
+    except Exception as e:
+        """ Hadnle exceptions by printing the errors and put them in the response """
+        print(e)
+        response = jsonify({'error': 'An error occurred while deleting team'})
+        response.status_code = 500
+        return response
+
+# update team data
+@bp.route('/update', methods=['PUT'])
+def update_team():
+
+    try:
+        data = request.json
+        team_id = data['TeamID']
+        team_name = data['TeamName']
+
+        query = "UPDATE Team TeamName=%s WHERE TeamID=%s"
+        execute_query(query, (team_name, team_id), commit=True)
+
+        response = jsonify({'message': 'Team information updated successfully'})
+        response.status_code = 200
+        return response
+    except Exception as e:
+        """ Hadnle exceptions by printing the errors and put them in the response """
+        print(e)
+        response = jsonify({'error': 'An error occurred while updating team information'})
+        response.status_code = 500
+        return response
+    
+# add member to the team
+@bp.route('/team/<int:team_id>/member', methods=['POST'])
+def add_member():
+
+    try:
+        data = request.json
+        member_id = data['StudentID']
+        team_id = ['TeamID']
+
+        query = "INSERT INTO members (member_id, team_id) VALUES (%s, %s)"
+        execute_query(query, (member_id, team_id), commit=True)
+        """ If inserted succesfuly return a message"""
+        response = jsonify({'message': 'Member inserted successfully'})
+        response.status_code = 200
+        return response
+    except Exception as e:
+        """ Hadnle exceptions by printing the errors and put them in the response """
+        print(e)
+        response = jsonify({'error': 'An error occurred while inserting member'})
+        response.status_code = 500
+        return response
+    
+# Get team members
+@bp.route('/team/<int:team_id>/member', methods=['GET'])
+def get_members(student_id):
+
+    try:
+        query = "SELECT StudentId FROM TeamMEmbership WHERE TeamId=%s"
+        members_row = execute_query(query, (student_id), fetch_one=True)
+        if members_row:
+            response = jsonify(members_row)
+            response.status_code = 200
+            return response
+        else:
+            return jsonify({"message": "Members not found"}), 404
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "Internal Server Error"}), 500
+
+
+# add project to the team
+@bp.route('/team/<int:team_id>/project', methods=['POST'])
+def add_project():
+
+    try:
+        data = request.json
+        project_id = data['ProjectID']
+        team_id = ['TeamID']
+
+        query = "INSERT INTO Collabration (project_id, team_id) VALUES (%s,%s)"
+        execute_query(query, (project_id), commit=True)
+        """ If inserted succesfuly return a message"""
+        response = jsonify({'message': 'Project inserted successfully'})
+        response.status_code = 200
+        return response
+    except Exception as e:
+        """ Hadnle exceptions by printing the errors and put them in the response """
+        print(e)
+        response = jsonify({'error': 'An error occurred while inserting project'})
+        response.status_code = 500
+        return response
+    
+# Get team projects
+@bp.route('/team/<int:team_id>/project', methods=['GET'])
+def get_project(project_id):
+
+    try:
+        query = "SELECT PROJECTID FROM Collaboration WHERE TeamID=%s"
+        projects_row = execute_query(query, (project_id), fetch_one=True)
+        if projects_row:
+            response = jsonify(projects_row)
+            response.status_code = 200
+            return response
+        else:
+            return jsonify({"message": "Projects not found"}), 404
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "Internal Server Error"}), 500
